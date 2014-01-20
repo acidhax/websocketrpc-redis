d220d63f685bd54c3f948f9e8bbd4b94ae9327c1
Mathieuu Gosbee <mail@matbee.com>
2014-01-20 01:37:02 -0500

    Added wormholeRedisRoom support

--- index.js
+++ index.js
@@ -9,12 +9,26 @@ if (fs.existsSync('../websocketrpc')) {
 var wormholeredis = function (redisSub) {
 	wormhole.call(this);
 	this.redisSub = redisSub;
+	this.rooms = [];
 };
 wormholeredis.prototype.__proto__ = wormhole.prototype;
-wormholeredis.prototype.joinRoom = function(room) {
+wormholeredis.prototype.joinRoom = function(room, cb) {
 	// Subscribe to room in redis.
+	var subRoom = new wormholeredisRoom(room);
+	this.rooms.push(subRoom);
+	cb && cb(subRoom);
 };
 wormholeredis.prototype.leaveRoom = function (rooom) {
 	// Unsubscribe from room in redis.
+};
+wormholeredis.prototype.onRoomMessage = function(room) {
+	// Parse room message.
+};
+
+var wormholeredisRoom = function (name) {
+	this.name = room;
+};
+wormholeredisRoom.prototype.onRoomMessage = function () {
+	// Parse room message.
 };
 module.exports = wormholeredis;

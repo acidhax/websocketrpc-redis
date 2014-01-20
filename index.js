9c31a0a6826fc1d71690727b41224d95aa91979f
Mathieuu Gosbee <mail@matbee.com>
2014-01-20 01:33:13 -0500

    Join room stubs

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ var wormholeredis = function () {
 	wormhole.call(this);
 };
 wormholeredis.prototype.__proto__ = wormhole.prototype;
+wormholeredis.prototype.joinRoom = function(room) {
+	// Subscribe to room in redis.
+};
+wormholeredis.prototype.leaveRoom = function (rooom) {
+	// Unsubscribe from room in redis.
+};
+module.exports = wormholeredis;
